34feb8c0e8b942aa6c4f4e1fb500ae8f29f475a4
Tushar <tusharshokeen32@gmail.com>
2021-09-10 13:53:58 +0530

    Add git ignore file

--- app.js
+++ app.js
@@ -135,8 +135,13 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port 8000");
 });
 
 app.get('/favicon.ico', (req, res) => res.status(204).end());
